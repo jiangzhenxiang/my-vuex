99027becefcf9e1e9ed7e8b5e027e007426dbeab
姜振祥 <jiangzx@jiedaibao.com>
2020-04-13 18:03:29 +0800

    1、简易vuex

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
 <template>
   <div id="app">
-    hello
+    hello,world
+    我今天{{$store.state.age}}
+
+    <button @click="syncAdd">syncAdd</button>
+    <button @click="asyncAdd">asyncAdd</button>
+
+    <hr>
+
+    gettersAge:{{$store.getters.gettersAge}}
+
+
+    <hr>
+    <HelloWorld></HelloWorld>
   </div>
 </template>
 
 <script>
-
+import HelloWorld from "./components/HelloWorld";
 export default {
   name: 'App',
+  components:{
+    HelloWorld
+  },
+  mounted() {
+    console.log('store', this.$store)
+  },
+  methods:{
+    syncAdd() {
+      this.$store.commit('syncAdd', 1)
+    },
+    asyncAdd() {
+      this.$store.dispatch('asyncAdd', 10)
+    }
+  }
 }
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
+// import Vuex from 'vuex';
+import Vuex from '../vuex';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        age: 18,
+    },
+    strict:true,
+    getters:{ // computed
+        gettersAge(state){ // 以前用vue中的计算属性
+            return state.age + '岁'
+        }
+    },
+    mutations: {
+        syncAdd(state, payload) {
+            state.age += payload;
+        }
+    },
+    actions: {
+        asyncAdd({commit}, payload) {
+            setTimeout(() => {
+                commit('syncAdd', payload)
+            }, 500);
+        }
+    },
+    modules: {}
 })
